b8fe6611ee2b94ebc3536ea254c2397f12d64c81
Ammar2882 <ammar.cnation@gmail.com>
2022-08-24 13:15:45 +0500

    notification finished

--- src/config/cronJobs.ts
+++ src/config/cronJobs.ts
@@ -7,29 +7,35 @@ import { sendNotificationMultipleDevices } from "./notification";
 
 //time 300
 export const sendNewsNotifications = async () => {
-  schedule.scheduleJob('*/30000 * * * * *', async () => {
+  schedule.scheduleJob('*/300 * * * * *', async () => {
     if (db.readyState === 1) {
-      console.log('preparing to notify')
-      const promises = await Promise.all([
-        MobileUser.find({ phoneOtp: "####", fcmToken: { $ne: "#" } }, { _id: 0, fcmToken: 1 }),
-        News.find({ isNotified: false, isDeleted: false }, { title: 1 })
-      ])
-      const users = promises[0]
-      const news = promises[1]
-      console.log("news: ", news, "users : ", users)
-      // let fcmTokens = []
-      // for (let i = 0; i < users.length; i++) {
-      //   fcmTokens.push(users[i].fcmToken)
-      // }
-      // for (let j = 0; j < news.length; j++) {
-      //   await sendNotificationMultipleDevices(fcmTokens, news[j].title)
-      // }
-      // // console.log(news)
-      // console.log('The answer to life, the universe, and everything!');
+      try{
+        const promises = await Promise.all([
+          MobileUser.find({ phoneOtp: "####", fcmToken: { $ne: "#" } }, { _id: 0, fcmToken: 1 }),
+          News.find({ isNotified: false, isDeleted: false }, { title: 1 })
+        ])
+        const users = promises[0]
+        const news = promises[1]
+        let fcmTokens = []
+        for (let i = 0; i < users.length; i++) {
+          if (users[i]?.fcmToken && users[i]?.fcmToken !== '') {
+            fcmTokens.push(users[i].fcmToken)
+          }
+        }
+        for (let j = 0; j < news.length; j++) {
+          if(news[j]?.title && news[j]?.title !== '' ){
+            await sendNotificationMultipleDevices(fcmTokens, news[j].title)
+            await News.findOneAndUpdate({_id:news[j]._id} , {isNotified:true})
+          }
+        }
+      }
+      catch(e){
+        console.log(e , "exception occurred")
+      }
+      
     }
     else {
       console.log('db not connected yet')
-      return
     }
   })
 }
